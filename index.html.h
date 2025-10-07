abf03723e362a2d2db13e50dd4d160ceb0ee096d
Neenu Sara Saji <103787973+neenu04@users.noreply.github.com>
2025-10-07 17:32:45 -0400

    Add files via upload

---
+++ index.html.h
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My Website</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+  <header>
+    <h1>Curations</h1>
+    <p>By Neenu Sara</p>
+  </header>
+
+  <main>
+    <section>
+      <h2>About My Gift Hampers</h2>
+      <p>Unwrap happiness with Curations , We create personalized gifts & hampers for every occasionthat matters.</p>
+    </section>
+
+    <section class="gallery">
+      <div class="item">
+        <img src="images/hamper1.jpg" alt="Festive Hamper">
+        <p>Festive Hamper</p>
+      </div>
+      <div class="item">
+        <img src="images/hamper2.jpg" alt="Birthday Hamper">
+        <p>Birthday Hamper</p>
+      </div>
+      <div class="item">
+        <img src="images/hamper3.jpg" alt="Special Occasion Hamper ">
+        <p>Special Occasion Hamper</p>
+      </div>
+      <div class="item">
+        <img src="images/hamper3.jpg" alt="Themed Hampers ">
+        <p>Themed Hamper</p>
+      </div>
+    </section>
+  </main>
+
+  <footer>
+    <p>© 2025 Curations Gift Hampers | Made with ❤️</p>
+  </footer>
+
+</body>
+</html>
---
+++ style.css.c
@@ -0,0 +1,30 @@
+body {
+  font-family: Arial, sans-serif;
+  background-color: #fff8f2;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+  color: #4a3c38;
+}
+
+header {
+  background-color: #f6d3c1;
+  padding: 20px;
+}
+
+.gallery {
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  margin: 20px;
+}
+
+.item img {
+  width: 200px;
+  border-radius: 10px;
+}
+
+footer {
+  background-color: #f6d3c1;
+  padding: 10px;
+}
